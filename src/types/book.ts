49233a70a7c32d9784c59f12272159787ae9079f
Valeria <lerik13@gmail.com>
2026-02-02 09:47:08 -0700

    action SearchBooks

---
+++ src/types/book.ts
@@ -0,0 +1,26 @@
+export interface Book {
+  cover_i: number
+  has_fulltext: boolean
+  edition_count: number
+  title: string
+  author_name: string[]
+  first_publish_year: number
+  key: string
+  ia: string[]
+  author_key: string[]
+  public_scan_b: boolean
+  ratings_average: number | null
+}
+
+export interface BookResponse {
+  start: number
+  numFoundExact: boolean
+  num_found: number
+  docs: Book[]
+}
+
+export interface CoverResponse {
+  source_url: string | null
+  width: number
+  height: number
+}
